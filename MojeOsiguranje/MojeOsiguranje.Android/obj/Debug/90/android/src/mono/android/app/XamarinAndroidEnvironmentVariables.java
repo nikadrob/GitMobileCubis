8ad1070f73ad191be81a55ab12621f9b895681ef
snasteski <nikadrob@live.com>
2019-05-31 16:08:35 +0200

    Info Page

--- MojeOsiguranje/MojeOsiguranje.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ MojeOsiguranje/MojeOsiguranje.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "11a620ee-83fc-4d06-b1b3-aa58089a4fcf",
+		"XAMARIN_BUILD_ID", "9f42c565-f029-46aa-884a-26dbac355f52",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
---
+++ MojeOsiguranje/MojeOsiguranje/Pages/InfoPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MojeOsiguranje.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InfoPage : ContentPage
+    {
+        public InfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapFacebook_OnTapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.facebook.com/dunavinsurance"));
+        }
+
+        private void TapTwitter_OnTapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://twitter.com/osigurani"));
+        }
+
+        private void TapInstagram_OnTapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.instagram.com/dunavosiguranje/"));
+        }
+
+        private void TapLinkedIn_OnTapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.linkedin.com/company/kompanija-dunav-osiguranje-ado"));
+        }
+
+        private void TapYoutube_OnTapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.youtube.com/user/DunavInsurance/videos?view=0"));
+        }
+
+        private void TapCall_OnTapped(object sender, EventArgs e)
+        {
+            //PhoneDialer.Open("996545231232");
+        }
+    }
+}
